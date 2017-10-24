0932b86cda3ee1752267562057dea26e496fb1a2
fushiqi <sf2788@columbia.edu>
2017-10-24 19:19:22 -0400

    new features

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require("express"),
 // Create a client instance
 let AWS = require('aws-sdk');
 AWS.config.update({
-    credentials: new AWS.Credentials('AKIAIAD5C3VLZFIUN7UA', '4n8CAPqZEgo6JJZTjN9lKvsCOwhp+x3VjiX7+0V3'),
+    credentials: new AWS.Credentials('AKIAIBUZAVSA4PYNR6EA', 'Q8e6gouOfr2CPTa2+MVlzeisgeR8DkZ6SPoMc1fj'),
     region: 'us-east-2'
 });
 let options = {
@@ -16,8 +16,10 @@ let es = require('elasticsearch').Client(options);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static("views"));
+app.use(express.static(__dirname + '/views'));
 
+
+// dropdown list search different markers
 app.get("/keyword", function(req, res){
     var searchWord = req.query.keyword;
     es.search({
@@ -40,8 +42,51 @@ app.get("/keyword", function(req, res){
     });
 });
 
+
+// realize elastic search 
+app.get("/geospatial", function(req, res){
+    es.search({
+        index: 'twitters',
+        type: 'tweet',
+        body: {
+            query: {
+                bool: {
+                    must: {
+                        match_all: {}
+                    },
+                    filter: {
+                        geo_distance: {
+                            distance: "1000km",
+                            geo: {
+                                lat: req.query.lat,
+                                lon: req.query.lng
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        size: 1000
+    }).then(function(esRes){
+        console.log('Number of tweets retrieved: ' + esRes.hits.total);
+        var hits = esRes.hits.hits;
+        res.contentType('application/json');
+        res.send(JSON.stringify(hits));
+    },function(err){
+        console.trace(err.message);
+    });
+});
+
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/introduction", function(req, res){
+    res.render("introduction");
+})
+
+app.get("/elastic", function(req, res){
+    res.render("elasticsearch");
+})
 
 app.listen(8081, '127.0.0.1');
