2f0bb53837a735ee7ceed58bf179594fe58f41a1
RanLI2836 <RanLI2836@users.noreply.github.com>
2017-11-19 00:29:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var express = require("express"),
 // Create a client instance
 let AWS = require('aws-sdk');
 AWS.config.update({
-    credentials: new AWS.Credentials('AKIAIBUZAVSA4PYNR6EA', 'Q8e6gouOfr2CPTa2+MVlzeisgeR8DkZ6SPoMc1fj'),
+    credentials: new AWS.Credentials('', ''),
     region: 'us-east-2'
 });
 let options = {
-  hosts: [ 'https://search-cc-twitter-py2dgskljuk7wo3ve5gmux3q4q.us-east-2.es.amazonaws.com' ], // array of amazon es hosts (required)
+  hosts: [ '' ], // array of amazon es hosts (required)
   connectionClass: require('http-aws-es'), // use this connector (required)
 };
 let es = require('elasticsearch').Client(options);
